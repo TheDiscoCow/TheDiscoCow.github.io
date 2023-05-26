84a48813136b7d293fd2fe6ad6e6936f15970cd7
TheDiscoCow <102273381+TheDiscoCow@users.noreply.github.com>
2023-05-26 18:04:42 +1000

    rustica-cucina website upload

---
+++ js/javascript.js
@@ -0,0 +1,12 @@
+function openModal(modalId) {
+    // Get the modal element
+    var modal = document.getElementById(modalId);
+
+    // Display the modal
+    modal.style.display = "block";
+}
+
+function closeModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "none";
+  }
